ee04d69d209216ece21628f00c2fd4928a08d69d
joaofxp <joaofxp@hotmail.com>
2016-11-03 17:35:36 -0200

    Mute Button

---
+++ Assets/AudioMute.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AudioMute : MonoBehaviour {
+
+    public static AudioMute singleton;
+
+    public Sprite MuteSprite;
+    public Sprite UnMuteSprite;
+
+    // Use this for initialization
+    void Start () {
+        singleton = this;
+	}
+	
+    public void AudioToggle()
+    {
+        GetComponentInParent<Button>().onClick.Invoke();
+        if (AudioListener.volume == 0)
+        {
+            AudioListener.volume = 1;
+            GetComponent<Image>().sprite = UnMuteSprite;
+        } else
+        {
+            AudioListener.volume = 0;
+            GetComponent<Image>().sprite = MuteSprite;
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    //Acionar o botão de mutar audio ao olhar e clicar
+    //Gero um raycast
+    //Aperto o botão
+    //Chama a função ou clica o botão
+
+    //Componentes
+    public Camera mainCamera;
+
+    void Awake()
+    {
+        mainCamera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.JoystickButton0))
+        {
+            Debug.Log("Key");
+        }
+
+        Ray ray = mainCamera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.tag == "ButtonMute" && Input.GetKeyDown(KeyCode.JoystickButton0))
+            {
+                //AudioListener.volume = 0;
+                AudioMute.singleton.AudioToggle();
+            }
+        }
+        //if (Physics.Raycast(ray, out hit))
+        //{
+        //if (hit.transform.tag == "Teleport" && !coroutineIsRunning)
+        //{
+        //    StartCoroutine(ChooseIngredient());
+        //    StartCoroutine(transform.GetComponentInChildren<LifeBar>().ShowLoader());
+
+        //}
+        //else if (hit.transform.tag == "Point")
+        //{
+        //    Debug.Log(hit.transform.tag);
+        //    foreach (GameObject ingredient in points)
+        //    {
+        //        ingredient.SetActive(false);
+        //    }
+        //    potionButton.SetActive(false);
+        //    potionComplete.SetActive(true);
+        //    potionText.text = "1/1";
+        //    potionSell.SetActive(true);
+        //}
+        //else if (hit.transform.tag == "Sell")
+        //{
+        //    potionComplete.SetActive(false);
+        //    endText.SetActive(true);
+        //}
+        //}
+    }
+}
