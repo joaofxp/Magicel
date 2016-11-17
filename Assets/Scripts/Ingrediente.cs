8ccd580f1449627e82137d553809a1f0ed6dafd7
joaofxp <joaofxp@hotmail.com>
2016-11-17 14:54:38 -0200

    Ingrediente Fade

--- Assets/Scripts/Ingrediente.cs
+++ Assets/Scripts/Ingrediente.cs
@@ -14,6 +14,7 @@ public class Ingrediente : MonoBehaviour
     public float ingredienteVelocidade = 50;
     Quaternion rotation;
     Vector3 posInicial;
+    bool travarRotacao;
 
     void Awake()
     {
@@ -23,6 +24,7 @@ public class Ingrediente : MonoBehaviour
         m_rigidbody = GetComponent<Rigidbody>();
         rotation = transform.rotation;
         posInicial = transform.position;
+        travarRotacao = false;
     }
 
     void FixedUpdate()
@@ -43,7 +45,11 @@ public class Ingrediente : MonoBehaviour
         {
             SelecaoDesativar();
         }
-        transform.rotation = rotation;
+
+        if (travarRotacao)
+        {
+            transform.rotation = rotation;
+        }
     }
 
     public void IngredienteSelecionado()
@@ -65,20 +71,20 @@ public class Ingrediente : MonoBehaviour
 
     public void SelecaoAtivar()
     {
+        travarRotacao = true;
         m_renderer.material.color = Color.green;
         transform.SetParent(Camera.main.transform);
         m_rigidbody.useGravity = false;
-        //m_rigidbody.freezeRotation = true;
         m_rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
         m_selecionado = true;
     }
 
     public void SelecaoDesativar()
     {
+        travarRotacao = false;
         m_renderer.material.color = m_mainColor;
         transform.SetParent(null);
         m_rigidbody.useGravity = true;
-        //m_rigidbody.freezeRotation = false;
         m_rigidbody.constraints = RigidbodyConstraints.None;
         m_selecionado = false;
         Destroy(transform.GetChild(0).gameObject);
@@ -89,8 +95,18 @@ public class Ingrediente : MonoBehaviour
     {
         yield return new WaitForSeconds(2);
         Renderer m_renderer = GetComponent<Renderer>();
-        m_renderer.material.color -= new Color(0, 0, 0, 1);
-        //transform.position = posInicial;
+        while (m_renderer.material.color.a >= 0)
+        {
+            m_renderer.material.color -= new Color(0, 0, 0, 0.25f);
+            yield return new WaitForSeconds(0.25f * Time.deltaTime);
+        }
+        transform.position = posInicial;
+        yield return new WaitForSeconds(1);
+        while (m_renderer.material.color.a <= 1)
+        {
+            m_renderer.material.color += new Color(0, 0, 0, 0.1f);
+            yield return new WaitForSeconds(0.25f * Time.deltaTime);
+        }
         //m_renderer.material.color += new Color(0, 0, 0, 1);
         yield return null;
     }
